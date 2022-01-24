aaa34d4292f238fbddfb3d15420529d08f2dc046
BitencourT <thayanebitencourt.ssa@gmail.com>
2022-01-24 11:22:37 -0300

    primeiro commit

---
+++ pythonExercicio/aula06a.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um valor: '))
+print(type(n1))
+n2 =int(input('Digite um valor: '))
+s = n1 + n2
+print('A soma entre',n1,'e', n2,'vale = ',s)
+#ou
+print('A soma entre {} e {} vale = {}'.format(n1,n2,s))
---
+++ pythonExercicio/ex003.py
@@ -0,0 +1,6 @@
+n=int(input('Informe um numero: '))
+n2=int(input('Informe outro numero: '))
+
+s=n+n2
+
+print('A soma de {} e {} é {}'.format(n,n2,s))
